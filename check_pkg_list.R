bce8f9eb7dfa5a672ce2c78c328c28e055f0756b
Scott Chamberlain <myrmecocystus@gmail.com>
2018-11-14 10:04:11 -0800

    add R script for checking for new packages to track

---
+++ check_pkg_list.R
@@ -0,0 +1,22 @@
+library(jsonlite)
+library(dplyr)
+
+# pkgs we're tracking
+using <- tolower(sort(readLines("packages.txt")))
+
+# pkgs we're not going to track
+donttrack <- tolower(sort(readLines("dont-track.txt")))
+
+# combine
+all <- unique(c(using, donttrack))
+
+url <- "https://raw.githubusercontent.com/ropensci/roregistry/gh-pages/registry.json"
+df <- fromJSON(url)
+(pkgs <- tbl_df(df$packages) %>%
+  # filter(on_cran, !cran_archived) %>%
+  filter(!cran_archived) %>%
+  filter(name != "pleiades") %>%
+  .$name %>% sort %>% tolower)
+
+# compare
+cat(pkgs[!pkgs %in% all], sep = "\n")
